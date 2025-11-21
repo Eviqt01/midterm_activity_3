bdb70c21073a7b2802fb8b17770bb033a76bd95b
Eviqt01 <izoooIz00013@gmail.com>
2025-11-22 00:40:22 +0800

    add clear button and fixed background image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,23 +6,35 @@
 		const data = await response.json();
 		joke = `${data.setup} - ${data.punchline}`;
 	};
+
+	const clear = () => {
+		joke = '';
+	};
 </script>
 
 <main
-	class="bg-[url(/src/img/lmao.jpeg)] bg-no-repeat bg-cover min-h-screen flex items-center justify-center flex-col gap-7"
+	class="bg-[url(/src/img/lmao.jpeg)] bg-fixed relative bg-no-repeat bg-cover min-h-screen flex items-center justify-center flex-col gap-7"
 >
 	<h1 class="text-2xl font-bold font-sans text-center">Random Joke <br /> Generator</h1>
 	<section
-		class="p-10 bg-black rounded-md border shadow flex flex-col items-center gap-4 min-w-100 w-50 sm:min-w-100 sm:w-50"
+		class="p-10 relative bg-black rounded-md border shadow flex flex-col items-center gap-4 sm:min-w-100 sm:w-50"
 	>
 		<span class=" text-5xl">😂</span>
 		<span class="text-white text-center font-sans">{joke}</span>
-		<button
-			class="px-8 py-2 bg-orange-300 font-bold cursor-pointer active:scale-95 hover:bg-blue-300 border rounded-sm text-sm"
-			onclick={getJoke}
-		>
-			Get A Random Joke
-		</button>
+		<div class="flex flex-col flex-wrap gap-2">
+			<button
+				class="px-8 py-2 bg-orange-300 font-bold cursor-pointer active:scale-95 hover:bg-orange-500 border rounded-sm text-sm"
+				onclick={getJoke}
+			>
+				Get A Random Joke
+			</button>
+			<button
+				class="px-8 py-2 bg-gray-300 font-bold cursor-pointer active:scale-95 hover:bg-gray-500 border rounded-sm text-sm"
+				onclick={clear}
+			>
+				Clear Button
+			</button>
+		</div>
 	</section>
 </main>
 <footer class="bg-gray-300 flex flex-col items-center gap-5 p-10">
